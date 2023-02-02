1dc843fa9da9091b833dd2a3689018832921b8a1
Bas Dijkstra <bas@ontestautomation.com>
2023-02-02 09:13:22 +0100

    Add request matching and fault examples

--- WireMockNetWorkshop/Examples/Examples02.cs
+++ WireMockNetWorkshop/Examples/Examples02.cs
@@ -1,6 +1,8 @@
 using NUnit.Framework;
+using System;
 using System.Net;
 using System.Threading.Tasks;
+using WireMock.Matchers;
 using WireMock.RequestBuilders;
 using WireMock.ResponseBuilders;
 using WireMock.Server;
@@ -21,11 +23,82 @@ namespace WireMockNetWorkshop.Examples
         private void StubUrlMatching()
         {
             server.Given(
-                Request.Create().WithPath("/hello-world").UsingGet()
+                Request.Create().WithPath("/url-matching").UsingGet()
             )
             .RespondWith(
                 Response.Create()
                 .WithBody("URL matching")
+            );
+        }
+
+        private void StubHeaderMatching()
+        {
+            server.Given(
+                Request.Create().WithPath("/header-matching").UsingGet()
+                .WithHeader("header_name", new ExactMatcher("header_value"))
+            )
+            .RespondWith(
+                Response.Create()
+                .WithBody("Header matching")
+            );
+        }
+
+        private void StubCookieMatching()
+        {
+            server.Given(
+                Request.Create().WithPath("/cookie-matching").UsingGet()
+                .WithCookie("cookie_name", new ExactMatcher("cookie_value"))
+            )
+            .RespondWith(
+                Response.Create()
+                .WithBody("Cookie matching")
+            );
+        }
+
+        private void StubJsonBodyMatching()
+        {
+            server.Given(
+                Request.Create().WithPath("/json-body-matching").UsingGet()
+                .WithBody(new JmesPathMatcher("fruit == 'banana'"))
+                .WithBody(new JmesPathMatcher("contains(date, '2023')"))
+            )
+            .RespondWith(
+                Response.Create()
+                .WithBody("JSON request body matching")
+            );
+        }
+
+        private void StubReturnErrorStatusCode()
+        {
+            server.Given(
+                Request.Create().WithPath("/error-status-code").UsingGet()
+            )
+            .RespondWith(
+                Response.Create()
+                .WithStatusCode(500)
+            );
+        }
+
+        private void StubReturnResponseWithDelay()
+        {
+            server.Given(
+                Request.Create().WithPath("/delayed-response").UsingGet()
+            )
+            .RespondWith(
+                Response.Create()
+                .WithStatusCode(200)
+                .WithDelay(TimeSpan.FromMilliseconds(2000))
+            );
+        }
+
+        private void StubReturnResponseWithFault()
+        {
+            server.Given(
+                Request.Create().WithPath("/fault-response").UsingGet()
+            )
+            .RespondWith(
+                Response.Create()
+                .WithFault(FaultType.EMPTY_RESPONSE)
             );
         }
 
