72e6edd70bd5225ce06bf9f4dc4f04eaf982e036
Bas Dijkstra <bas@ontestautomation.com>
2023-03-30 14:54:17 +0200

    Remove RestSharp as a dependency

--- WireMockNetWorkshop/TestBase.cs
+++ WireMockNetWorkshop/TestBase.cs
@@ -1,6 +1,5 @@
 using NUnit.Framework;
 using RestAssured.Request.Builders;
-using RestSharp;
 using WireMock.Server;
 
 namespace WireMockNetWorkshop
@@ -8,17 +7,13 @@ namespace WireMockNetWorkshop
     public class TestBase
     {
         protected WireMockServer server;
-        protected RestClient client;
 
         protected RequestSpecification requestSpec;
-
-        private const string BASE_URL = "http://localhost:9876";
 
         [SetUp]
         public void StartServer()
         {
             server = WireMockServer.Start(9876);
-            client = new RestClient(BASE_URL);
 
             requestSpec = new RequestSpecBuilder()
                 .WithHostName("localhost")
